b42166a1dd77544f83344ca9c673beb601fe9d4a
chizhipeng <375032565@qq.com>
2019-10-01 21:02:41 +0800

    写了一个洪乞丐讨钱的问题

--- src/com/czp/Technological/ForTest.java
+++ src/com/czp/Technological/ForTest.java
@@ -15,6 +15,22 @@ public class ForTest {
 			System.out.println("for 循环输出" + j);
 		}
 
+		//----------------分割线--------------乞丐
+		/*
+		 * 天朝有一个乞丐姓洪，去天桥要钱
+第一天要了1块钱
+第二天要了2块钱
+第三天要了4块钱
+第四天要了8块钱
+以此类推
+
+问题： 洪乞丐干10天，收入是多少？
+		 */
+		int sumMoney = 0;
+		for(int j = 0;j<10;j++) {
+			sumMoney = sumMoney + (int)Math.pow(2, j);
+		}
+		System.out.println("最后乞丐的收入是" + sumMoney);
 	}
 
 }
