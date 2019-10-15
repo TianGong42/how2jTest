15d8ffd2fe80d298b437fe712b2a4fb5c8885ca8
chizhipeng <375032565@qq.com>
2019-10-15 22:57:42 +0800

    123

--- src/com/czp/Technological/IsLeapYear.java
+++ src/com/czp/Technological/IsLeapYear.java
@@ -6,7 +6,7 @@ public class IsLeapYear {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.println("请输入年份");
+		System.out.println("请输入年份    ");
 		int year = input.nextInt();
 		if(year%4==0 & year % 100 != 0 && year % 400 ==0) {
 			System.out.println(year + "是闰年");
